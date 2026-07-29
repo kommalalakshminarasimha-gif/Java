90d6f1edbc25bc1ecb8a259f05a4e337c259113d
kommalalakshminarasimha-gif <kommala.lakshminarasimha@gmail.com>
2026-07-29 16:48:36 +0530

    new one

--- javademo/src/com/javaintro/Task7.java
+++ javademo/src/com/javaintro/Task7.java
@@ -1,10 +1,16 @@
 package com.javaintro;
 
 public class Task7 {
+	
+	int stuId;
+	String stuName;
 
 	public static void main(String[] args) {
-		
-
+		Task7 a = new Task7();
+		a.stuId = 100;
+		a.stuName ="Narsimha";
+		System.out.println("Id is :" + a.stuId);
+		System.out.println("My Name is  : " + a.stuName);
 	}
 
 }
