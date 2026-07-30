58fcb5a077af0587003fc4375ad89576f3f2c0e8
kommalalakshminarasimha-gif <kommala.lakshminarasimha@gmail.com>
2026-07-30 15:04:16 +0530

    New one

--- javademo/src/com/javaintro/Task7.java
+++ javademo/src/com/javaintro/Task7.java
@@ -2,15 +2,54 @@ package com.javaintro;
 
 public class Task7 {
 	
-	int stuId;
-	String stuName;
+	//Static variables
+	
+	static  String Library ="KLN";
+	static String LibarianName ="Narasimha";
 
-	public static void main(String[] args) {
-		Task7 a = new Task7();
-		a.stuId = 100;
-		a.stuName ="Narsimha";
-		System.out.println("Id is :" + a.stuId);
-		System.out.println("My Name is  : " + a.stuName);
+	// Instance varible 
+	int bookId;
+	String bookTitle;
+	String author_Name;
+	int avilable_copie;
+
+	//Intsant block
+	void Bookdetails() {
+		System.out.println("Book Id : " + bookId);
+		System.out.println("BookTitle : " + bookTitle);
+		System.out.println("AuthorName : " + author_Name);
+		System.out.println("Avilable Copies : " + avilable_copie );
 	}
-
+	
+	//Static block
+	
+	static void LibraryDetails(){
+		System.out.println("Library Name : " + Library);
+		System.out.println("LibarianName : " + LibarianName);
+		System.out.println("----------------------------------");
+		
+	}
+	public static void main(String[] args) {
+		//Object creation 
+		Task7 a = new Task7();
+		Task7 b = new Task7();
+		
+		a.bookId =10;
+		a.bookTitle ="Men";
+		a.author_Name ="KL.Narsimha";
+		a.avilable_copie = 24;
+		a.Bookdetails();
+		a.LibraryDetails();
+		
+		
+		//Displa details
+		
+		b.bookId =11;
+		b.bookTitle ="Women";
+		b.author_Name ="Lakshmi";
+		b.avilable_copie=16;
+		b.Bookdetails();
+		b.LibraryDetails();
+	
+	}
 }
