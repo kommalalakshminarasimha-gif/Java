f8e8a27d42ac6e395d76c25d090ca02268b2f25c
kommalalakshminarasimha-gif <kommala.lakshminarasimha@gmail.com>
2026-08-03 11:34:16 +0530

    new one

---
+++ javademo/src/Task8.java
@@ -0,0 +1,18 @@
+
+public class Task8 {
+	int a =10;
+	int b = 20;
+	
+	void abc(){
+		
+		int c = a+b;
+		System.out.println(c);
+		
+	}
+	public static void main(String[] args) {
+		Task8 t = new Task8(); 
+		t.abc();
+	    System.out.println("Narasimha" );  
+	}
+
+}
