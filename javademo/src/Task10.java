c86aa639d7b9a539f77ddc94f0a2437a1758e013
kommalalakshminarasimha-gif <kommala.lakshminarasimha@gmail.com>
2026-08-05 09:31:38 +0530

    new one

---
+++ javademo/src/Task10.java
@@ -0,0 +1,50 @@
+
+
+public class Task10 {
+	
+	String movie_name;
+	int seat_number;
+	String booking_status;
+	static String theatre_name = "Sai ranga";
+	int Ticket_id;
+	boolean stores;
+	void book_ticket() {
+		System.out.println("bookin status : " + booking_status);
+	}
+	void cancel_ticket() {
+		System.out.println("bookin status : " + booking_status);
+	}
+	void display_ticket_details() {
+		System.out.println("Ticket Id is : " + Ticket_id);
+		System.out.println("Ticket_stored  : " + stores);
+		System.out.println("Movie name is : " + movie_name);
+		System.out.println("seat_number is : " + seat_number);
+		System.out.println("Theatre_name is : " + theatre_name);
+	}
+	public static void main(String[]args) {
+		Task10 t1 = new Task10();
+		t1.movie_name = "Spider_man";
+		t1.seat_number = 18;
+		t1.booking_status = "Reserved";
+		t1.stores = true;
+		t1.Ticket_id = 101;
+		t1.book_ticket();
+		t1.display_ticket_details();
+		System.out.println("Your ticket is booked successfully");
+		System.out.println("*********************************************");
+		
+		
+		Task10 t2 = new Task10();
+		theatre_name = "GPR multiplex";
+		t2.movie_name = "Chennai love stroy";
+		t2.seat_number = 12;
+		t2.booking_status = "Not reserved";
+		t2.stores = false;
+		t2.Ticket_id = 102;
+		t2.book_ticket();
+		t2.display_ticket_details();
+		System.out.println("Your ticket booking is cancelled");
+	}
+
+	
+}
--- javademo/src/Task9.java
+++ javademo/src/Task9.java
@@ -1,9 +1,56 @@
 
 public class Task9 {
-
+	
+	static String theatreName = "KLN Theater";
+	
+	String movieName;
+	int seatNumber;
+	String bookingStatus;
+	int TicketId;
+	boolean stores;
+	
+	void bookTicket() {
+		System.out.println("Bookin status : " + bookingStatus);
+	}
+	
+	void cancelTicket() {
+		System.out.println("Bookin status : " + bookingStatus);
+		
+	}
+ 	
+	void display_ticket_details() {
+		System.out.println("Ticket Id is : " + TicketId);
+		System.out.println("Ticket_stored  : " + stores);
+		System.out.println("Movie name is : " + movieName);
+		System.out.println("seat_number is : " + seatNumber);
+		System.out.println("Theatre_name is : " + theatreName);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Task9 a = new Task9();
+		a.movieName = "Devara";
+		a.seatNumber = 3;
+		a.bookingStatus ="Reserved" ;
+		a.stores = true;
+		a.TicketId = 11;
+		a.bookTicket();
+		a.cancelTicket();
+		a.display_ticket_details();		
+		System.out.println("---------------------");
+		
+		
+		Task9 b = new Task9();
+		b.movieName = "Devara";
+		b.seatNumber = 3;
+		b.bookingStatus ="Reserved" ;
+		b.stores = true;
+		b.TicketId = 11;
+		b.bookTicket();
+		b.cancelTicket();
+		b.display_ticket_details();		
+		
+		
+		
 	}
 
 }
