64a321f4d135b02547aebfa2436eb311b7439a70
kommalalakshminarasimha-gif <kommala.lakshminarasimha@gmail.com>
2026-08-10 17:31:07 +0530

    new one

---
+++ javademo/src/com/javaintro/Test12.java
@@ -0,0 +1,11 @@
+package com.javaintro;
+
+public class Test12 {
+
+	public static void main(String[] args) {
+		byte b = 120;
+		byte c = (byte) (b+12);
+		System.out.println(b);
+	}
+
+}
--- javademo/src/com/langfundumentals/DataTypesDemo1.java
+++ javademo/src/com/langfundumentals/DataTypesDemo1.java
@@ -3,30 +3,71 @@ package com.langfundumentals;
 //Primitive data Types
 
 public class DataTypesDemo1 {
-	
-	byte b = 100;
-	short s;
-	int i;
-	long l;
-	
-	float f;
-	double d;
-	
-	char c;
-	boolean boo;
-	
-	public static void main(String[] args) {
+
+
+	//note : By default RHS values are int type
+ 
+		// the byte min-max range is -128 to 127
+		byte b = 127;
 		
-		DataTypesDemo1 a = new DataTypesDemo1();
+		//CE : type mismatch convert from int to byte
+		byte b1 = (byte) 128;
 		
-		System.out.println("byte Value :" +a.b);//0
-		System.out.println("Short value :" +a.s);//0
-		System.out.println("Int Value :" +a.i);//0
-		System.out.println("Long Value :" +a.l);//0
-		System.out.println("Float Value :" + a.f);//0.0
-		System.out.println("Double Value :" + a.d);//0.0
-		System.out.println("Char Value :" + a.c);//
-		System.out.println("Boolean Value :" + a.boo);//false
+		//-128, -127, -126, -125 ...... 0 1 2 3 4.... 126 127 
+		short s  = b1; //converting byte to short 
+		
+		//short means 2 bytes = 2^15
+		//range : -32768 to 32767  
+		short s1 = -32767;
+		
+		
+		//int means means 4 bytes = 32bits = 2^31 =  -2147483648 to 2147483647 to
+		
+		int i = 2147483647;
+		
+		//the literal 2147483647 of type int is out of range
+		int i1  = (int) 2147483648L;
+		
+		//long means 8 bytes = 64bits = 2^63 = 
+		long l = 2147483647;
+		long l1 = -9223372036854775808L;
+		long l2 = 9223372036854775807L;
+		
+		//float means 4 bytes
+		//double means 8 bytes
+		//by default values
+		float f = 5.9f;
+		double d = 6.5d;
+		
+		
+		//char means 2 bytes = 16bits = 32768 + 32767 = 65535
+		//the range of char is 0 to 65535
+		char c = 'A';
+		char c1 = 'M';
+		
+		char c2 = 65; // ASCII codes ----> 65 to 90 = A to Z 
+	    char c3 = 97;//  ASCII codes ----> 97 to 122 = a to z
+	    char c4 = '\u0040' ; //unicodes
+	    int i5 = 'C';
+		boolean bool = true;
+		
+		short s2 = (short)32768;
+		 public  static void main(String[] args) {
+			 DataTypesDemo1 t = new DataTypesDemo1 ();
+			 System.out.println(t.s);
+			 System.out.println(t.s1);
+			 System.out.println(t.s2);
+			 System.out.println(t.i);
+			 System.out.println(t.i1);
+			 System.out.println(t.f);
+			 System.out.println(t.d);
+			 System.out.println(t.c);
+			 System.out.println(t.c1);
+			 System.out.println(t.c2);
+			 System.out.println(t.c3);
+			 System.out.println(t.c4);
+			 System.out.println(t.i5);
+			 System.out.println(t.bool);
+		 }
 	}
 
-}
